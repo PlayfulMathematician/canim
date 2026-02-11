a1271934c7e183e192e6b4f222b2b05209e9067c
Playful Mathematician <me@playfulmathematician.com>
2026-02-11 13:52:17 +0800

    asdfasdfasdfasdfasdfasdfadsfadsf

--- include/canim/math.h
+++ include/canim/math.h
@@ -136,3 +136,69 @@ CANIM_API CanimVec3 canim_lerp_vec3(CanimVec3 a, CanimVec3 b, CanimNumber t);
 /// @param b The second
 /// @return The vector
 CANIM_API CanimVec3 canim_cross_vec3(CanimVec3 a, CanimVec3 b);
+
+/// @brief This adds vec's (CanimVec2 or CanimVec3, it is generic)
+/// @param a The first
+/// @param b The second
+/// @return The vector
+#define canim_add_vec(a, b)                                                    \
+  _Generic((a), CanimVec2: canim_add_vec2, CanimVec3: canim_add_vec3)((a), (b))
+
+/// @brief This subtracts vec's (CanimVec2 or CanimVec3, it is generic)
+/// @param a The first
+/// @param b The second
+/// @return The vector
+#define canim_sub_vec(a, b)                                                    \
+  _Generic((a), CanimVec2: canim_sub_vec2, CanimVec3: canim_sub_vec3)((a), (b))
+
+/// @brief This computes the magnitude of vec's (CanimVec2 or CanimVec3, it is
+/// generic)
+/// @param a The Vector
+/// @return The magnitude
+#define canim_magnitude_vec(a)                                                 \
+  _Generic((a),                                                                \
+      CanimVec2: canim_magnitude_vec2,                                         \
+      CanimVec3: canim_magnitude_vec3)((a))
+
+/// @brief This normalizes vec's (CanimVec2 or CanimVec3, it is
+/// generic)
+/// @param a The Vector
+/// @return The normalized version
+#define canim_normalize_vec(a)                                                 \
+  _Generic((a),                                                                \
+      CanimVec2: canim_normalize_vec2,                                         \
+      CanimVec3: canim_normalize_vec3)((a))
+
+/// @brief This computes the dot product (CanimVec2 or CanimVec3, it is
+/// generic)
+/// @param a The Vector
+/// @param b The second vector
+/// @return The dot product
+#define canim_dot_vec(a, b)                                                    \
+  _Generic((a), CanimVec2: canim_dot_vec2, CanimVec3: canim_dot_vec3)((a), (b))
+
+/// @brief This scales a vec (CanimVec2 or CanimVec3, it is
+/// generic)
+/// @param a The Vector
+/// @param b The scaler
+/// @return The scaling
+#define canim_scale_vec(a, b)                                                  \
+  _Generic((a), CanimVec2: canim_scale_vec2, CanimVec3: canim_scale_vec3)((a), \
+                                                                          (b))
+
+/// @brief This negates a vec (CanimVec2 or CanimVec3)
+/// @param a The vector
+/// @return The negation
+#define canim_negative_vec(a)                                                  \
+  _Generic((a),                                                                \
+      CanimVec2: canim_negative_vec2,                                          \
+      CanimVec3: canim_negative_vec3)((a))
+
+/// @brief This lerps a vec (CanimVec2 or CanimVec3)
+/// @param a The vector
+/// @param b The second vector
+/// @param t the inbetweeness
+/// @return The lerp
+#define canim_lerp_vec(a, b, t)                                                \
+  _Generic((a), CanimVec2: canim_lerp_vec2, CanimVec3: canim_lerp_vec3)(       \
+      (a), (b), (t))
--- tests/test_vec.c
+++ tests/test_vec.c
@@ -8,11 +8,17 @@
 
 static void test_vec_zero(void **state) {
   (void)state;
-  CanimVec2 v = {.x = 0, .y = 1};
-  CanimVec2 u = {.x = 1, .y = 0};
-  CanimVec2 sum = canim_add_vec2(u, v);
-  assert_float_equal(sum.x, 1.0f, 1e-6);
-  assert_float_equal(sum.y, 1.0f, 1e-6);
+  CanimVec2 v2 = {.x = 0, .y = 1};
+  CanimVec2 u2 = {.x = 1, .y = 0};
+  CanimVec2 sum2 = canim_add_vec(u2, v2);
+  CanimVec3 v3 = {.x = 0, .y = 1, .z = 1};
+  CanimVec3 u3 = {.x = 1, .y = 0, .z = 0};
+  CanimVec3 sum3 = canim_add_vec(u3, v3);
+  assert_float_equal(sum2.x, 1.0f, 1e-6);
+  assert_float_equal(sum2.y, 1.0f, 1e-6);
+  assert_float_equal(sum3.x, 1.0f, 1e-6);
+  assert_float_equal(sum3.y, 1.0f, 1e-6);
+  assert_float_equal(sum3.z, 1.0f, 1e-6);
 }
 
 int main(void) {
