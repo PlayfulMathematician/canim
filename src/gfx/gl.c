1991e90847bbf3bbaf735a5fd091310d15b84be2
Playful Mathematician <me@playfulmathematician.com>
2026-03-30 17:03:55 +0800

    i am an authoritarian government
    let's go i did it i am so hpapy, the sgadre cmoplse

--- src/gfx/gl.c
+++ src/gfx/gl.c
@@ -45,16 +45,20 @@ struct CanimGfxDevice {
 const char *VS_LIST[CANIM_LAST_SHADER] = {
     "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 v_position;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   v_position = gl_Position;\n"
     "}\0"};
+
 const char *FS_LIST[CANIM_LAST_SHADER] = {
     "#version 330 core\n"
     "out vec4 FragColor;\n"
+    "in vec4 v_position;\n"
     "void main()\n"
     "{\n"
-    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "   FragColor = vec4(0.0f, abs(v_position.y), 0.2f, 1.0f);\n"
     "}\0"};
 // add logging
 GLuint compile_shaders(CanimLogger *c_log, const char *shader, GLenum type) {
